209c105bb3ed3e73525ce35d3f5ce8c05c3f98cf
Samiha <samiha17@vt.edu>
2025-11-08 23:23:20 -0500

    added conversion program

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# example-repo
+# Project
+
+Conversion Program
+
+# Description
+
+This Python program demonstrates type conversion between different data types — integers, floats, and strings.
---
+++ conversion.py
@@ -0,0 +1,14 @@
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+number_int = int(num1)
+number_float = float(num2)
+number_string = str(num3)
+string_int = int(string1)
+
+print(number_int)
+print(number_float)
+print(number_string)
+print(string_int)
